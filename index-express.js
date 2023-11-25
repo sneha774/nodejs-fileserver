28cad9eab0157f0eaf9f8b2679528243dd2794df
Snehal Sangam <sneha774@icloud.com>
2023-11-25 16:53:25 -0600

    included express router

--- index-express.js
+++ index-express.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+const apiRoot = '/api/v1';
+
+const package = require('./package.json');
 
 // Use body-parser middleware to parse incoming request bodies
 app.use(bodyParser.json());
@@ -11,9 +14,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use CORS middleware to handle Cross-Origin Resource Sharing
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!!!');
+// Configure Routes
+const router = express.Router();
+router.get('/', (req, res) => {
+    const packageDescription = {
+        name: package.name,
+        version: package.version,
+        description: package.description
+    };
+    res.send(`Welcome to ${packageDescription.name} v${packageDescription.version}!`);
 });
+app.use(apiRoot, router);
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
