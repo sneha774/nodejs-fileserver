00139eefbc48c0604aa7dc51e89c4eccd85f01f0
Snehal Sangam <sneha774@icloud.com>
2023-11-25 17:08:53 -0600

    added a fake json db and  a username query route

--- index-express.js
+++ index-express.js
@@ -14,8 +14,41 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use CORS middleware to handle Cross-Origin Resource Sharing
 app.use(cors());
 
+// Sample User Accounts Database
+const db = {
+    'jdoe': {
+        username: 'jdoe',
+        name: 'John Doe',
+        id: 1,
+        description: 'John Doe is a fictional character',
+        currency: 'USD',
+        balance: 100.00,
+        transactions: []
+    },
+    'bsmith': {
+        username: 'bsmith',
+        name: 'Bob Smith',
+        id: 2,
+        description: 'Bob Smith is a fictional character',
+        currency: 'USD',
+        balance: 100.00,
+        transactions: []
+    },
+    'jbloggs': {
+        username: 'jbloggs',
+        name: 'Joe Bloggs',
+        id: 3,
+        description: 'Joe Bloggs is a fictional character',
+        currency: 'USD',
+        balance: 100.00,
+        transactions: []
+    }
+}
+
+
 // Configure Routes
 const router = express.Router();
+
 router.get('/', (req, res) => {
     const packageDescription = {
         name: package.name,
@@ -24,6 +57,17 @@ router.get('/', (req, res) => {
     };
     res.send(`Welcome to ${packageDescription.name} v${packageDescription.version}!`);
 });
+
+router.get('/accounts/:username', (req, res) => {
+    const username = req.params.username;
+    const account = db[username];
+    if (account) {
+        res.json(account);
+    } else {
+        res.status(404).send(`Account ${username} not found`);
+    }
+});
+
 app.use(apiRoot, router);
 
 
