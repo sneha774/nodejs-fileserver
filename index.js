7c11824f93d2716338b95ce99ede80216548652d
Snehal Sangam <sneha774@icloud.com>
2023-11-25 09:03:50 -0600

    sales report demo

--- index.js
+++ index.js
@@ -2,33 +2,75 @@ const fs = require('fs').promises;
 const path = require('path');
 
 async function main() {
-    const currentDir = path.resolve(__dirname);
-    const allFiles = await GetAllFiles(currentDir);
-    ListFiles(allFiles);
+    const storesDir = path.join(__dirname, 'stores');
+    const storeSalesFiles = await GetAllFiles(storesDir);
+    console.log('All Store Sales files:');
+    ListFiles(storeSalesFiles);
+
+    const totalSales = await CalculateTotalSales(storeSalesFiles);
+    console.log(`Total Sales: ${totalSales}`);
+
+    await WriteSalesReport(totalSales, storeSalesFiles);
 }
 main();
 
 async function GetAllFiles(dirPath) {
-    const files = await fs.readdir(dirPath);
-    const allFiles = [];
+    const items = await fs.readdir(dirPath, { withFileTypes: true });
+    const files = [];
 
-    for (const file of files) {
-        const fullPath = path.join(dirPath, file);
-        const fileStat = await fs.stat(fullPath);
-        if (fileStat.isDirectory()) {
-            if (file !== 'node_modules' && file !== '.git') {
+    for (const item of items) {
+        const fullPath = path.join(dirPath, item.name);
+        if (item.isDirectory()) {
+            if (item !== 'node_modules' && item !== '.git') {
                 const subDirFiles = await GetAllFiles(fullPath);
-                allFiles.push(...subDirFiles);
+                files.push(...subDirFiles);
             }
         } else {
-            allFiles.push(fullPath);
+            if (path.extname(fullPath) === '.json') {
+                files.push(fullPath);
+            }
         }
     }
-    return allFiles;
+    return files;
 }
 
 async function ListFiles(allFiles) {
     for (const file of allFiles) {
-        console.log(`\t${file}`);
+        console.log(`- ${file}`);
     }
+}
+
+async function CalculateTotalSales(storeSalesFiles) {
+    let totalSales = 0;
+    for (const file of storeSalesFiles) {
+        const storeSales = await fs.readFile(file);
+        const salesData = JSON.parse(storeSales);
+        totalSales += salesData.total;
+    }
+    return totalSales;
+}
+
+async function WriteSalesReport(totalSales, storeSalesFiles) {
+    const report = {
+        totalSales: totalSales,
+        totalStores: storeSalesFiles.length
+    };
+
+    const storeTotalsDir = path.join(__dirname, 'salesTotals');
+    try {
+        await fs.mkdir(storeTotalsDir);
+    }
+    catch (err) {
+        console.error(`Creating directory: ${err.message}`);
+    }
+
+    const reportFilePath = path.join(storeTotalsDir, 'report.json');
+    try {
+        await fs.unlink(reportFilePath);
+    }
+    catch (err) {
+        console.error(`Deleting file: ${err.message}`);
+    }
+    fs.writeFile(reportFilePath, JSON.stringify(report, null, 2));
+    console.log(`Sales report writen to ${reportFilePath}`);
 }
