45ef9ae0e4eec4315b5fe66c2244d0341e9e4215
Snehal Sangam <sneha774@icloud.com>
2023-11-30 09:20:30 -0600

    Generated comments for index-node methods

--- index-node.js
+++ index-node.js
@@ -14,6 +14,11 @@ async function main() {
 }
 main();
 
+/**
+ * Retrieves all files with a .json extension from the specified directory and its subdirectories.
+ * @param {string} dirPath - The path of the directory to search for files.
+ * @returns {Promise<string[]>} - A promise that resolves to an array of file paths.
+ */
 async function GetAllFiles(dirPath) {
     const items = await fs.readdir(dirPath, { withFileTypes: true });
     const files = [];
@@ -34,12 +39,22 @@ async function GetAllFiles(dirPath) {
     return files;
 }
 
+/**
+ * Lists all files in the given array.
+ * @param {Array<string>} allFiles - The array of file names.
+ * @returns {Promise<void>} - A promise that resolves when all files have been listed.
+ */
 async function ListFiles(allFiles) {
     for (const file of allFiles) {
         console.log(`- ${file}`);
     }
 }
 
+/**
+ * Calculates the total sales from an array of store sales files.
+ * @param {string[]} storeSalesFiles - The array of file paths to store sales files.
+ * @returns {Promise<number>} The total sales.
+ */
 async function CalculateTotalSales(storeSalesFiles) {
     let totalSales = 0;
     for (const file of storeSalesFiles) {
@@ -71,6 +86,7 @@ async function WriteSalesReport(totalSales, storeSalesFiles) {
     catch (err) {
         console.error(`Deleting file: ${err.message}`);
     }
-    fs.writeFile(reportFilePath, JSON.stringify(report, null, 2));
+
+    await fs.writeFile(reportFilePath, JSON.stringify(report, null, 2));
     console.log(`Sales report writen to ${reportFilePath}`);
 }
